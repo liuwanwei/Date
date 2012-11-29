15142bc352843f39c0f429360d354ba22ef6c08f
mao yu <maoyu417@163.com>
2012-11-29 09:10:56 +0800

    添加type字段
    用于日后区分用户来源，如是新浪、facebook

--- BilateralFriend.h
+++ BilateralFriend.h
@@ -2,7 +2,7 @@
 //  BilateralFriend.h
 //  Date
 //
-//  Created by maoyu on 12-11-12.
+//  Created by maoyu on 12-11-29.
 //  Copyright (c) 2012年 Liu&Mao. All rights reserved.
 //
 
@@ -12,12 +12,13 @@
 
 @interface BilateralFriend : NSManagedObject
 
-@property (nonatomic, retain) NSNumber * userID;
-@property (nonatomic, retain) NSString * nickname;
 @property (nonatomic, retain) NSString * imageUrl;
 @property (nonatomic, retain) NSNumber * isOnline;
 @property (nonatomic, retain) NSNumber * isRead;
 @property (nonatomic, retain) NSString * lastReminderID;
+@property (nonatomic, retain) NSString * nickname;
 @property (nonatomic, retain) NSNumber * unReadRemindersSize;
+@property (nonatomic, retain) NSNumber * userID;
+@property (nonatomic, retain) NSNumber * type;
 
 @end
--- BilateralFriend.m
+++ BilateralFriend.m
@@ -2,7 +2,7 @@
 //  BilateralFriend.m
 //  Date
 //
-//  Created by maoyu on 12-11-12.
+//  Created by maoyu on 12-11-29.
 //  Copyright (c) 2012年 Liu&Mao. All rights reserved.
 //
 
@@ -11,12 +11,13 @@
 
 @implementation BilateralFriend
 
-@dynamic userID;
-@dynamic nickname;
 @dynamic imageUrl;
 @dynamic isOnline;
 @dynamic isRead;
 @dynamic lastReminderID;
+@dynamic nickname;
 @dynamic unReadRemindersSize;
+@dynamic userID;
+@dynamic type;
 
 @end
