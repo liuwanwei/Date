41a38f37f46b66c913e6c2f9df3a4af6021b835d
mao yu <maoyu417@163.com>
2012-11-30 08:46:33 +0800

    添加新文件，引起工程文件修改

---
+++ Date/ReminderNotificationDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReminderNotificationDetailViewController.h
+//  Date
+//
+//  Created by lixiaoyu on 12-11-29.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReminderNotificationDetailViewController : UIViewController
+
+@end
---
+++ Date/ReminderNotificationDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  ReminderNotificationDetailViewController.m
+//  Date
+//
+//  Created by lixiaoyu on 12-11-29.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import "ReminderNotificationDetailViewController.h"
+
+@interface ReminderNotificationDetailViewController ()
+
+@end
+
+@implementation ReminderNotificationDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
