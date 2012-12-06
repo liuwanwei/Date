5fea45e38231371c6917f9f1ffbbadb8ef7e7230
mao yu <maoyu417@163.com>
2012-12-06 18:00:39 +0800

    添加字段

---
+++ Date/Reminder.m
@@ -0,0 +1,27 @@
+//
+//  Reminder.m
+//  date
+//
+//  Created by lixiaoyu on 12-12-6.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import "Reminder.h"
+
+
+@implementation Reminder
+
+@dynamic audioUrl;
+@dynamic desc;
+@dynamic id;
+@dynamic isBell;
+@dynamic isRead;
+@dynamic latitude;
+@dynamic longitude;
+@dynamic sendTime;
+@dynamic triggerTime;
+@dynamic type;
+@dynamic userID;
+@dynamic audioTime;
+
+@end
