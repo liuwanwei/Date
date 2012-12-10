daf0bdcb24da439b4469f1e04217da72a408cb9a
mao yu <maoyu417@163.com>
2012-12-10 18:24:44 +0800

    添加新文件

---
+++ Date/ReminderDetailAudioCell.h
@@ -0,0 +1,17 @@
+//
+//  ReminderDetailAudioCell.h
+//  date
+//
+//  Created by maoyu on 12-12-10.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ReminderBaseCell.h"
+
+@interface ReminderDetailAudioCell : ReminderBaseCell
+
+@property (weak, nonatomic) IBOutlet UILabel * labelTitle;
+@property (weak, nonatomic) IBOutlet UILabel * labelDesc;
+
+@end
