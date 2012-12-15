c9a180ee886fde4a04a797f580ed70c738979eb8
mao yu <maoyu417@163.com>
2012-12-15 09:53:12 +0800

    项目新文件

---
+++ Date/ReminderSettingTimeCell.h
@@ -0,0 +1,25 @@
+//
+//  ReminderSettingTimeCell.h
+//  date
+//
+//  Created by maoyu on 12-12-14.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol ReminderSettingTimeCellDelegate <NSObject>
+
+@optional
+- (void)valueChangedWithSwitch:(UISwitch *)sender;
+@end
+
+@interface ReminderSettingTimeCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel * labelTitle;
+@property (weak, nonatomic) IBOutlet UISwitch * switchTime;
+
+@property (weak, nonatomic) id<ReminderSettingTimeCellDelegate> delegate;
+
+- (IBAction)valueChanged:(UISwitch *)sender;
+@end
